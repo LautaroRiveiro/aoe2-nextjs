4fcf166b33e554dea1d62c7eb67c8f24b5a58df4
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-04-29 19:22:15 -0300

    Miduhack para no cargar de calls a api gratuita

--- services/civilizations.ts
+++ services/civilizations.ts
@@ -1,12 +1,16 @@
 import axios from "axios";
-import { Civilization, CivilizationResponse } from "../interfaces";
+import fs from 'fs/promises';
+import { Civilization } from "../interfaces";
 
 const aoe2API = axios.create({
   baseURL: 'https://age-of-empires-2-api.herokuapp.com/api/v1'
 })
 
 const getAllCivilizations = async (): Promise<Civilization[]> => {
-  const {data} = await aoe2API.get<CivilizationResponse>('/civilizations')
+  // const {data} = await aoe2API.get<CivilizationResponse>('/civilizations')
+  // Local fetch to avoid API Calls
+  const content = await fs.readFile('./services/civilizations.json', 'utf-8')
+  const data = JSON.parse(content)
   return data.civilizations
 }
 
