55692613be32534d2789b48516d6c655fc5f3d34
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-05-03 10:54:00 -0300

    open graph metatags

--- layouts/AppLayout.tsx
+++ layouts/AppLayout.tsx
@@ -7,6 +7,8 @@ interface Props {
   civilization?: string
 }
 
+const origin = typeof window === 'undefined' ? '' : window.location.origin
+
 export const AppLayout: FC<PropsWithChildren<Props>> = ({ children, civilization }) => {
   return (
     <Container xl css={{ w: '100%', p: 0, minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
@@ -19,6 +21,9 @@ export const AppLayout: FC<PropsWithChildren<Props>> = ({ children, civilization
             : "Civilizaciones del AOE2"
         } />
         <meta name="keywords" content={"aoe2, civilizaciones" + (civilization ? `, ${civilization}` : '')} />
+        <meta property="og:title" content={civilization ? `La civilización ${civilization}` : "Civilizaciones del AoE2"} />
+        <meta property="og:description" content={civilization ? `Información sobre los ${civilization}` : "Información sobre las civilizaciones del AoE2"} />
+        <meta property="og:image" content={`${origin}/banner.jpg`} />
       </Head>
       <Navbar />
       <Container as='main' css={{ flex: 1, paddingBottom: '16px' }}>
