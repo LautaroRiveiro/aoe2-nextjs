be855cc01a79fef9fda526db7e0953a26f359a5b
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-05-02 22:22:09 -0300

    NextUI dark mode

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,20 @@
-import { Container, Row, Avatar, Link } from "@nextui-org/react"
+import { Container, Row, Avatar, Link, Switch, useTheme, Spacer } from "@nextui-org/react"
 import NextLink from "next/link"
+import { useTheme as useNextTheme } from 'next-themes'
+import { SunIcon } from '../styles/SunIcon'
+import { MoonIcon } from '../styles/MoonIcon'
 
 const Navbar = () => {
+
+  const { setTheme } = useNextTheme()
+  const { isDark, theme } = useTheme()
+
   return (
-    <Container xl as='header' css={{ backgroundColor: '$primary' }}>
+    <Container
+      xl
+      as='header'
+      css={{ backgroundColor: isDark ? theme?.colors.backgroundContrast.value : theme?.colors.primary.value }}
+    >
       <Row css={{ h: '55px' }} justify='space-between' align='center'>
         <NextLink href="/" passHref>
           <Avatar
@@ -14,13 +25,21 @@ const Navbar = () => {
             textColor="primary"
           />
         </NextLink>
-        <nav>
+        <Container as="nav" display="flex" alignItems="center" justify="flex-end" css={{ p: 0 }}>
           <NextLink href="/favoritos" passHref>
-            <Link css={{color:"$white"}}>
+            <Link css={{ color: "$white" }}>
               Favoritos
             </Link>
           </NextLink>
-        </nav>
+          <Spacer />
+          <Switch
+            checked={isDark}
+            size="sm"
+            iconOn={<SunIcon filled />}
+            iconOff={<MoonIcon filled />}
+            onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
+          />
+        </Container>
       </Row>
     </Container>
   )
--- layouts/AppLayout.tsx
+++ layouts/AppLayout.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@nextui-org/react"
+import Head from "next/head"
 import { FC, PropsWithChildren } from "react"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
@@ -8,6 +9,9 @@ interface Props { }
 export const AppLayout: FC<PropsWithChildren<Props>> = ({ children }) => {
   return (
     <Container xl css={{ w: '100%', p: 0, minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <Head>
+        <title>AOE2 Civs</title>
+      </Head>
       <Navbar />
       <Container as='main' css={{ flex: 1, paddingBottom: '16px' }}>
         {children}
